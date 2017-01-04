0809486ba9b814db23b7dcd86df91561a7823f97
ki4070ma <ki4070ma@gmail.com>
2017-01-04 18:24:58 +0900

    Added q18-21

--- sand_box.py
+++ sand_box.py
@@ -92,49 +92,76 @@ def q9(sentence="I couldn't believe that I could actually understand what I was 
   print " ".join(new_sentence)
 
 def q10(input_file="hightemp.txt"):
-  import subprocess as sp
-  return int(sp.check_output("cat {} | wc -l".format(input_file), shell=True).strip())
+  cmd="cat {} | wc -l".format(input_file)
+  print execute_cmd(cmd)
 
 def q11():
-  import subprocess as sp
   # can be sed
   # expand -> expand -t 1 hightemp.txt
   cmd = "cat hightemp.txt | tr '\t' ' ' > ./q11/hightemp_space.txt"
-  sp.call(cmd, shell=True)
+  print execute_cmd(cmd)
 
 def q12():
-  import subprocess as sp
   for i in range(1, 3):
     cmd = "cat hightemp.txt | cut -f{0} > q12/col{0}.txt".format(i)
-    sp.call(cmd, shell=True)
+    print execute_cmd(cmd)
 
 def q13():
-  import subprocess as sp
   cmd = "paste q12/col1.txt q12/col2.txt > q13/col1_2.txt"
-  sp.call(cmd, shell=True)
+  print execute_cmd(cmd)
 
 def q14(n=10):
-  import subprocess as sp
   cmd="head -n{} hightemp.txt".format(n)
-  sp.call(cmd.split())
+  print execute_cmd(cmd)
 
 def q15(n=10):
-  import subprocess as sp
   cmd="tail -n{} hightemp.txt".format(n)
-  sp.call(cmd.split())
+  print execute_cmd(cmd)
 
 def q16(n=3):
-  import subprocess as sp
   # q10() -> number of lines in input_file
   input_file = "hightemp.txt"
   cmd="split -l {} {} {}".format(q10(input_file) // n, input_file, "q16/")
-  sp.call(cmd.split())
+  print execute_cmd(cmd)
 
 def q17():
-  import subprocess as sp
   cmd="cat hightemp.txt | awk '{print $1}' | sort | uniq | wc -l"
-  print int(sp.check_output(cmd, shell=True).strip())
+  print execute_cmd(cmd)
+
+def q18():
+  cmd="sort -r -nk3 {}".format("hightemp.txt")
+  print execute_cmd(cmd)
+
+def q19():
+  cmd="cut -f1 {} | sort | uniq -c | sort -nk1 -r | cut -f1".format("hightemp.txt")
+  print execute_cmd(cmd)
+
+def execute_cmd(cmd):
+  import subprocess as sp
+  return sp.check_output(cmd, shell=True).strip()
+
+def q20():
+  wikis = []
+  with open("jawiki-country.json", "r") as fp:
+    for line in fp:
+      wikis.append(line.strip())
+  for wiki in wikis:
+    if "イギリス" == eval(wiki)['title']:
+      return eval(wiki)['text']
+
+def q21():
+  text = q20()
+  lines = []
+  for line in text.split('\n'):
+    if "Category" in line:
+      lines.append(line)
+  return lines
+
+# def q22():
+#   lines = q21()
+#   for line in lines:
+#     print line
 
 if __name__ == '__main__':
-  q17()
+  q21()
 
